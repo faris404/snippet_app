2db0a0959d1e4dc0c0275f651b69f851890dace0
faris404 <faris.um2000@gmail.com>
2021-10-12 11:35:27 +0530

    model created

--- snippet_app/settings.py
+++ snippet_app/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'snippet_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'snippets_app',
+        'HOST':'127.0.0.1',
+        'USER':'faris404',
+        'PORT':'3306',
+        'PASSWORD':'12345678'
+
     }
+   
 }
 
-
+#  'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
 
 # Create your models here.
+class Snippets(models.Model):
+
+   title = models.CharField(max_length=50)
+   text = models.CharField(max_length=200)
+   create_at = models.DateTimeField()
+   user = models.ForeignKey(User)
+
+   class Meta:
+      db_table="snippets"
+class Tags(models.Model):
+   title = models.CharField(max_length=100,unique=True)
+   snippet = models.ManyToManyField(Snippets)
+
+   class Meta:
+      db_table="tags"
