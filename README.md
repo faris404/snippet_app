938657ea6b448840677be7d4f88b6b523fafef33
faris404 <faris.um2000@gmail.com>
2021-10-12 14:02:27 +0530

    readme changes

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ simple text storing app
 -  create virtualenv
 - install packages using `pip install -r requirements.txt`
 - migrate database and run
+- create super user from django admin
--- snippet_app/settings.py
+++ snippet_app/settings.py
@@ -88,19 +88,6 @@ DATABASES = {
    
 }
 
-#  'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'snippets_app',
-#         'HOST':'127.0.0.1',
-#         'USER':'faris404',
-#         'PORT':'3306',
-#         'PASSWORD':'12345678'
-
-#     }
-#  'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
